3ab66ec9227a381042e3f4a4a80ac1eced6758fa
Citizen01 <jeanbaptiste.e@hotmail.fr>
2015-03-05 22:21:49 +0100

    Created the visual studio project using mui

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace CS_Demo_Manager
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using FirstFloor.ModernUI.Windows.Controls;
+
+namespace CS_Demo_Manager
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : ModernWindow
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
